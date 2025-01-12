d7cd69f7b9220b17fd4616ddcaf563d3b6450cab
Absobel <imkonnu@gmail.com>
2025-01-12 18:23:14 +0100

    plugins

--- .other/obsidian-note-link-to-uri-link.sh
+++ .other/obsidian-note-link-to-uri-link.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-if [ -z "$1" ] || [ -z "$2" ]; then
-  echo "Usage: $0 <vault_path> <input_file>"
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <vault_path>"
   exit 1
 fi
 
-cd $1
+cd $1 || { echo "Error: vault path invalid"; exit 1; }
 vn=$(pwd | xargs basename)
-content=$(cat "$2")
+
+if read -t 0; then
+  content=$(cat -)
+else
+  echo "error: no content provided via stdin."
+  exit 1
+fi
 
 while IFS= read -r match; do
   safe=$(printf '%s' "$match" | sed 's/[.[\*^$(){}|?&+]/\\&/g')
@@ -15,6 +21,6 @@ while IFS= read -r match; do
   if [ -n "$path" ]; then
     content=$(echo "$content" | sed -E "s|\[\[${safe}\]\]|[${safe}](obsidian://open?vault=$vn\&file=${path})|g")
   fi
-done < <(echo "$content" | grep -oP '(?<=\[\[).*(?=\]\])')
+done < <(echo "$content" | grep -oP '(?<=\[\[)[^\]]+(?=\]\])')
 
 echo "$content"
